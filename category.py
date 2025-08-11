97039e4039db731a709a56422dd76d805a89d1fb
Bruno Mendes <brupmendes@icloud.com>
2025-08-11 20:49:54 -0300

    refactor: migrate Category class to use dataclasses

--- category.py
+++ category.py
@@ -1,21 +1,17 @@
 import uuid
+from uuid import UUID
+from dataclasses import dataclass, field
 
-class Category():
-    def __init__(
-            self, 
-            name, 
-            id = "", 
-            description = "",
-            is_active = True
-        ):
-        
-        self.id = id or uuid.uuid4()
-        self.name = name
-        self.description = description
-        self.is_active = is_active
+@dataclass
+class Category:
+    name: str
+    description: str = ""
+    is_active: bool = True
+    id: UUID = field(default_factory=uuid.uuid4)
 
+    def __post_init__(self):
         self.validate()
-        
+
     def __str__(self):
         return f"{self.name} - {self.description} ({self.is_active})"
     
