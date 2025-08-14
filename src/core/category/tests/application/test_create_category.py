d25c120c4fb1255c1d64ba5bf8ccccc8d4da849a
Bruno Mendes <brupmendes@icloud.com>
2025-08-13 21:07:13 -0300

    feat: add CreateCategory application class with unit tests

---
+++ src/core/category/tests/application/test_create_category.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock
+from uuid import UUID
+from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest
+
+import pytest
+
+from src.core.category.application.exceptions import InvalidCategoryData
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
+class TestCreateCategory:
+    def test_create_category_with_valid_data(self):
+        mock_repository = MagicMock(InMemoryCategoryRepository)
+        use_case = CreateCategory(repository=mock_repository)
+        request = CreateCategoryRequest(
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True #default
+        )
+
+        category_id = use_case.execute(request=request)
+
+        assert category_id is not None
+        assert isinstance(category_id, UUID)
+        assert mock_repository.save.called is True
+
+    def test_create_category_with_invalid_data(self):
+        mock_repository = MagicMock(InMemoryCategoryRepository)
+        use_case = CreateCategory(repository=mock_repository)
+        with pytest.raises(InvalidCategoryData, match="name cannot be empty.") as exc_info:
+            request = CreateCategoryRequest(
+                name=""
+            )
+            category_id = use_case.execute(request=request)
+
+        #ex de como pode ser feito    
+        assert exc_info.type is InvalidCategoryData
+        assert str(exc_info.value) == "name cannot be empty."
