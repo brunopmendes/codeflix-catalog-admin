71a36f40d41ebdaa92bf80b2a9aa8ba6b546ed5c
Bruno Mendes <brupmendes@icloud.com>
2025-08-20 21:10:04 -0300

    test: add integration and unit tests for CreateCategory functionality

--- src/core/category/tests/application/unit/test_create_category.py
+++ src/core/category/tests/application/unit/test_create_category.py
@@ -1,7 +1,7 @@
 from unittest.mock import MagicMock
 from uuid import UUID
 from src.core.category.application.category_repository import CategoryRepository
-from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest
+from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest, CreateCategoryResponse
 from src.core.category.application.exceptions import InvalidCategoryData
 
 import pytest
@@ -17,10 +17,11 @@ class TestCreateCategory:
             is_active=True #default
         )
 
-        category_id = use_case.execute(request=request)
+        response = use_case.execute(request=request)
 
-        assert category_id is not None
-        assert isinstance(category_id, UUID)
+        assert response.id is not None
+        assert isinstance(response, CreateCategoryResponse)
+        assert isinstance(response.id, UUID)
         assert mock_repository.save.called is True
 
     def test_create_category_with_invalid_data(self):
@@ -30,7 +31,7 @@ class TestCreateCategory:
             request = CreateCategoryRequest(
                 name=""
             )
-            category_id = use_case.execute(request=request)
+            response = use_case.execute(request=request)
 
         #ex de como pode ser feito    
         assert exc_info.type is InvalidCategoryData
