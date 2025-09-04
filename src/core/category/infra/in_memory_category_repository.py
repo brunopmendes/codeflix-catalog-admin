aa105a5d75fbdd9e0fce9553c442353eb8f8e73c
Bruno Mendes <brupmendes@icloud.com>
2025-09-04 20:04:13 -0300

    fix: ensure proper initialization of categories in InMemoryCategoryRepository

--- src/core/category/infra/in_memory_category_repository.py
+++ src/core/category/infra/in_memory_category_repository.py
@@ -5,7 +5,7 @@ from src.core.category.domain.category import Category
 
 class InMemoryCategoryRepository(CategoryRepository):
     def __init__(self, categories=None):
-        self.categories = categories or []
+        self.categories = categories or []  
 
     def save(self, category: Category) -> None:
         self.categories.append(category)
@@ -20,3 +20,11 @@ class InMemoryCategoryRepository(CategoryRepository):
         category = self.get_by_id(id)
         self.categories.remove(category)
 
+    def update(self, category: Category) -> None:
+        old_category = self.get_by_id(category.id)
+        if old_category:
+            self.categories.remove(old_category)
+            self.categories.append(category)
+
+    def list(self) -> list[Category]:
+        return [category for category in self.categories]    
