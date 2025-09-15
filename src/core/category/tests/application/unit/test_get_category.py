8550424f85200b535ac21d0915fb86de25ec2010
Bruno Mendes <brupmendes@icloud.com>
2025-09-15 20:02:41 -0300

    fix: update import paths for use cases in test_get_category.py

--- src/core/category/tests/application/unit/test_get_category.py
+++ src/core/category/tests/application/unit/test_get_category.py
@@ -1,12 +1,12 @@
 from unittest.mock import MagicMock, create_autospec
 from uuid import UUID
 from src.core.category.application.category_repository import CategoryRepository
-from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest, CreateCategoryResponse
-from src.core.category.application.exceptions import InvalidCategoryData
+from src.core.category.application.use_cases.create_category import CreateCategory, CreateCategoryRequest, CreateCategoryResponse
+from src.core.category.application.use_cases.exceptions import InvalidCategoryData
 
 import pytest
 
-from src.core.category.application.get_category import GetCategory, GetCategoryRequest, GetCategoryResponse
+from src.core.category.application.use_cases.get_category import GetCategory, GetCategoryRequest, GetCategoryResponse
 from src.core.category.domain.category import Category
 
 
